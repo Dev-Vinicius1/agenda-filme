8e4900ea62a34dc42d5b0f4c5f6fada41faeacfe
EyzRyder <gabriel.bessi.gb@gmail.com>
2024-09-04 22:56:54 -0300

    feat: criar esqueleto do projeto

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -9,50 +8,136 @@ public class Main {
 
         int opcao = 0;
 
+        Map<String,Runnable> exibir = new HashMap<String,Runnable>();
+        exibir.put("menu", Main::exibirComprarIngressoECadastro);
+        exibir.put("cadastro", Main::exibirCadastro);
+
         do {
-            exibirMenu();
+            //TODO add try catch because might show null value and not run
+            exibir.get(modoDeExibir).run();
+
             try {
                 opcao = Integer.parseInt(scanner.nextLine());
             } catch (Exception e) {
                 opcao = 0;
             }
 
-            switch (opcao) {
-                case 1:
-                    adicionarContato(scanner, contatos);
+            switch (modoDeExibir){
+                case "menu":
+                    opcaoComprarIngressoECadastro(opcao, scanner);
                     break;
-                case 2:
-                    detalharContato(scanner, contatos);
+                case "cadastro":
+                    opcaoCadastro(opcao, scanner,  contatos);
                     break;
-                case 3:
-                    editarContato(scanner, contatos);
-                    break;
-                case 4:
-                    removerContato(scanner, contatos);
-                    break;
-                case 5:
-                    listarContatos(contatos);
-                    break;
-                case 6:
-                    System.out.println("Saindo...");
-                    break;
-                default:
-                    System.out.println("Opção inválida!");
             }
-        } while (opcao != 6);
+        } while (opcao != 0);
 
         scanner.close();
     }
 
-    private static void exibirMenu() {
-        System.out.println("\n--- Menu da Agenda ---");
+    private static String modoDeExibir="menu";
+    private static Cliente clienteAtual = null;
+    private static List<Cliente> clientes = new ArrayList<>();
+
+    private static void exibirCadastro() {
+        //TODO mostrar certas coisas sw cliente for default ou funcionario
+        System.out.println("\n--- Cadastro ---");
         System.out.println("1. Adicionar Contato");
         System.out.println("2. Detalhar contato");
         System.out.println("3. Editar Contato");
         System.out.println("4. Remover Contato");
         System.out.println("5. Listar contatos");
-        System.out.println("6. Sair");
+        System.out.println("6. Voltar");
+        System.out.println("0. Sair");
         System.out.print("Escolha uma opção: ");
+    }
+
+    private static void opcaoCadastro(int opcao, Scanner scanner, Map<String, String[]> contatos){
+        switch (opcao) {
+            case 1:
+                //TODO
+                adicionarContato(scanner, contatos);
+                break;
+            case 2:
+                //TODO
+                detalharContato(scanner, contatos);
+                break;
+            case 3:
+                //TODO
+                editarContato(scanner, contatos);
+                break;
+            case 4:
+                //TODO
+                removerContato(scanner, contatos);
+                break;
+            case 5:
+                //TODO
+                listarContatos(contatos);
+                break;
+            case 6:
+                modoDeExibir="menu";
+                System.out.println("Voltando...");
+                break;
+            case 0:
+                System.out.println("Saindo...");
+                break;
+            default:
+                System.out.println("Opção inválida!");
+        }
+    }
+
+    private static void exibirComprarIngressoECadastro(){
+        //TODO
+        System.out.println("\n--- Auto Atendimento de Cinema ---");
+        System.out.println("1. Cadastrar Seu Contato");
+        System.out.println("2. Logar com seu Contato");
+        if(clienteAtual!=null){
+            System.out.println("3. Comprar ingresso");
+            System.out.println("4. Ver historico de ingressos");
+            System.out.println("5. Deslogar");
+        }
+        System.out.println("0. Sair");
+        System.out.print("Escolha uma opção: ");
+    }
+
+    private static void opcaoComprarIngressoECadastro(int opcao, Scanner scanner){
+        //TODO
+        switch (opcao) {
+            case 1:
+                modoDeExibir="cadastro";
+                break;
+            case 2:
+                logarCliente(scanner);
+                break;
+            case 3:
+                comprarIngresso(scanner);
+                break;
+            case 4:
+                exibirHistorico();
+                break;
+            case 5:
+                //TODO
+                clienteAtual=null;
+                break;
+            case 0:
+                System.out.println("Saindo...");
+                break;
+            default:
+                System.out.println("Opção inválida!");
+        }
+    }
+
+    private static void logarCliente(Scanner scanner) {
+        //TODO
+        clienteAtual = new ClienteDefault("nometest","11963079537","email@gmail.com");
+    }
+
+    private static void comprarIngresso(Scanner scanner) {
+        //TODO
+    }
+
+    private static void exibirHistorico() {
+        //TODO
     }
 
     private static void adicionarContato(Scanner scanner, Map<String, String[]> contatos) {
