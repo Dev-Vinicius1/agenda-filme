236389599bcc7f02b4281b638ea84b1da6571ff7
EyzRyder <gabriel.bessi.gb@gmail.com>
2024-09-06 23:09:32 -0300

    feat: cria class ingresso e fazer ele herdar de filme

--- src/Filme.java
+++ src/Filme.java
@@ -1,7 +1,8 @@
 
 public class Filme {
 
-    private  int id;
+    private static int quantidadeDeFilmesEIngressos;
+    private final int id;
     private String titulo;
     private int anoDeLancamento;
     private String categoria;
@@ -9,12 +10,30 @@ public class Filme {
     private int classificacaoEtaria;
 
 
-    public Filme(int id, String titulo, int anoDeLancamento, String categoria, String sinopse, int classificacaoEtaria) {
-       this.id = id;
+    public Filme(
+            String titulo,
+            int anoDeLancamento,
+            String categoria,
+            String sinopse,
+            int classificacaoEtaria
+    ) {
+        quantidadeDeFilmesEIngressos = quantidadeDeFilmesEIngressos +1;
+        this.id = quantidadeDeFilmesEIngressos;
         this.titulo = titulo;
         this.anoDeLancamento = anoDeLancamento;
         this.categoria = categoria;
         this.sinopse = sinopse;
         this.classificacaoEtaria = classificacaoEtaria;
+    }
+
+    @Override
+    public String toString() {
+        return "Filme{" +
+                "titulo='" + titulo + '\'' +
+                ", ano de lancamento=" + anoDeLancamento +
+                ", categoria='" + categoria + '\'' +
+                ", sinopse='" + sinopse + '\'' +
+                ", classificacao etaria=" + classificacaoEtaria +
+                '}';
     }
 }
--- src/Sala.java
+++
@@ -1,8 +0,0 @@
-//public class Sala extends Sessao{
-//
-//    private double preco;
-//
-//    public Sala(String titulo, int anoDeLancamento, String categoria, String sinopse, int classificacaoEtaria) {
-//        super(titulo, anoDeLancamento, categoria, sinopse, classificacaoEtaria);
-//    }
-//}
